e0148437fb7883858385f9fa1e1785d6bae76510
Aleksey Golodnikov <jusstes@yandex.ru>
2021-03-21 20:41:09 +0700

    add details for 5th sprint

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,8 @@ let editBtn = document.querySelector('.profile__about-edit');
 let nameInfo = document.querySelector('.profile__about-name');
 let jobInfo = document.querySelector('.profile__about-caption');
 let likes = document.querySelectorAll('.element__button-like');
+let addBtn = document.querySelector('.profile__add');
+
 
 editBtn.addEventListener('click', popupToggle);
 closeBtn.addEventListener('click', popupToggle);
@@ -26,7 +28,36 @@ function formSubmitHandler(evt) {
 }
 
 for (let i = 0; i < likes.length; i++) {
-  likes[i].addEventListener('click', function() {
-  likes[i].classList.toggle('element__button-like_active');
+  likes[i].addEventListener('click', function () {
+    likes[i].classList.toggle('element__button-like_active');
   });
 }
+
+const initialCards = [
+  {
+    name: 'Эгейское море',
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/markos-mant-unsplash.jpg'
+  },
+  {
+    name: 'Сибирь',
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/vadim-sadovski-unsplash.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/kirill-pershin-unsplash.jpg'
+  },
+  {
+    name: 'Сидней',
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/ivan-tsaregorodtsev-unsplash.jpg'
+  }
+  ,
+  {
+    name: 'Лондон',
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/watcharlie-unsplash.jpg'
+  }
+  ,
+  {
+    name: 'Калифорния',
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/logan-weaver-unsplash.jpg'
+  }
+]
