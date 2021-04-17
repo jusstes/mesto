832c4115f2d2853341ec3f7c337f9787895a580a
Aleksey Golodnikov <jusstes@yandex.ru>
2021-04-17 14:39:00 +0700

    add new js file for object

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,15 +1,3 @@
-const validateConfig = {
-  formSelector: '.form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__submit',
-  inactiveButtonClass: 'form__submit_disable',
-  inputErrorClass: 'form__input_type_error',
-  errorClass: 'popup__error_visible',
-  errorMessageEmptyInput: 'Вы пропустили это поле.',
-  errorMessageEmptyUrl: 'Введите адрес сайта.',
-  inputUrlClass: 'form__input_type_url'
-};
-
 const showInputError = (formElement, inputElement, validateConfig) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   setCustomError(formElement, inputElement, validateConfig);
