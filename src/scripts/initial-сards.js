5567ccfd59a9ff9c879743f48aac54b3bed35ff3
Aleksey Golodnikov <jusstes@yandex.ru>
2021-05-04 04:05:20 +0700

    fix: image link on gh

--- src/scripts/initial-сards.js
+++ src/scripts/initial-сards.js
@@ -3,28 +3,28 @@ export { initialCards };
 const initialCards = [
   {
     name: 'Эгейское море',
-    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/markos-mant-unsplash.jpg'
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/src/images/markos-mant-unsplash.jpg'
   },
   {
     name: 'Сибирь',
-    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/vadim-sadovski-unsplash.jpg'
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/src/images/vadim-sadovski-unsplash.jpg'
   },
   {
     name: 'Домбай',
-    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/kirill-pershin-unsplash.jpg'
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/src/images/kirill-pershin-unsplash.jpg'
   },
   {
     name: 'Сидней',
-    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/ivan-tsaregorodtsev-unsplash.jpg'
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/src/images/ivan-tsaregorodtsev-unsplash.jpg'
   }
   ,
   {
     name: 'Лондон',
-    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/watcharlie-unsplash.jpg'
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/src/images/watcharlie-unsplash.jpg'
   }
   ,
   {
     name: 'Калифорния',
-    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/images/logan-weaver-unsplash.jpg'
+    link: 'https://raw.githubusercontent.com/jusstes/mesto/main/src/images/logan-weaver-unsplash.jpg'
   }
 ];
